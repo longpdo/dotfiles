3429eb700e5dbe462256a0bd781481bfecff7a2d
Long Do <ldo@adorsys.de>
2020-05-16 19:51:15 +0200

    Set path to use ruby installed with brew

--- config/zsh/env.zsh
+++ config/zsh/env.zsh
@@ -7,12 +7,13 @@ export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.8.0_202.jdk/Contents/Ho
 export TERM="xterm-256color"
 
 # PATHS
-export PATH="${PATH}:/Users/longdo/.gem/ruby/2.7.0/bin"
-export PATH="${PATH}:/usr/local/opt/ruby/bin"
+# Set ruby installed by Homebrew first in your PATH
+export PATH="/usr/local/opt/ruby/bin:$PATH"
 export PATH="${PATH}:/usr/local/opt/coreutils/libexec/gnubin"
 export PATH="${PATH}:/usr/local/opt/findutils/libexec/gnubin"
 export PATH="${PATH}:/usr/local/opt/grep/libexec/gnubin"
 export PATH="${PATH}:/usr/local/opt/bc/bin"
 export PATH="${PATH}:/usr/local/opt/fzf/bin"
+export PATH="${PATH}:/Users/longdo/.gem/ruby/2.7.0/bin"
 export PATH="${PATH}:/Users/longdo/.local/bin"
 export PATH="${PATH}:/Users/longdo/repos/flutter/bin"
