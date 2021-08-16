92a40baa46d53a96b1a61b7904c1b7f07e9acafa
Ducane <saltukke@gmail.com>
2021-08-16 15:42:18 +0200

    Fixed and improved brew-install.sh

--- scripts/brew/brew-install.sh
+++ scripts/brew/brew-install.sh
@@ -4,7 +4,7 @@
 # > Select files with <Tab>
 # > Confirm with <Enter>
 
-_selected=$(brew search | fzf -m)
+_selected=$(brew search "$1" | tail -n +2 | fzf -m)
 
 if [[ $_selected ]]; then
   for brew in $_selected; do
